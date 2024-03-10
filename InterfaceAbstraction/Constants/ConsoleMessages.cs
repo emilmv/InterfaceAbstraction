137ed8baf91ff78013b48830778899fb616a65ab
emilmv <emilnm@code.edu.az>
2024-03-10 09:54:04 +0400

    Texts removed from Task1

---
+++ InterfaceAbstraction/Constants/ConsoleMessages.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAbstraction.Constants
+{
+    internal class ConsoleMessages
+    {
+        public const string MethodChoose = "Please choose method you want to operate:";
+        public const string SquareMethod = "1. Area of Square";
+        public const string RectangularMethod = "2. Area of Rectangular";
+        public const string ExitMethod = "3. Exit";
+        public const string SquareSide = "Please enter side of Square";
+        public const string RectangularWidth = "Please enter width of Rectangular";
+        public const string RectangularLength = "Please enter length of Rectangular";
+
+
+
+
+    }
+}
--- InterfaceAbstraction/Program.cs
+++ InterfaceAbstraction/Program.cs
@@ -1,4 +1,5 @@
-﻿using InterfaceAbstraction.Models;
+﻿using InterfaceAbstraction.Constants;
+using InterfaceAbstraction.Models;
 using System.Reflection.Metadata.Ecma335;
 
 namespace InterfaceAbstraction
@@ -7,32 +8,32 @@ namespace InterfaceAbstraction
     {
         static void Main(string[] args)
         {
+        #region Task1
         Beginning:
             string input;
             int userInput;
 
-            Console.WriteLine("Please choose method you want to operate:");
-            Console.WriteLine("1. Area of Square");
-            Console.WriteLine("2. Area of Rectangular");
-            Console.WriteLine("3. Exit");
+            Console.WriteLine(ConsoleMessages.MethodChoose);
+            Console.WriteLine(ConsoleMessages.SquareMethod);
+            Console.WriteLine(ConsoleMessages.RectangularMethod);
+            Console.WriteLine(ConsoleMessages.ExitMethod);
             bool isCorrectInput = int.TryParse(Console.ReadLine(), out userInput);
             if (!isCorrectInput) goto Beginning;
             switch (userInput)
             {
                 case 1:
                 areaOfSquare:
-                    Console.WriteLine("Please enter side of Square");
+                    Console.WriteLine(ConsoleMessages.SquareSide);
                     bool isCorrectSide = double.TryParse(Console.ReadLine(), out double squareSide);
                     if (!isCorrectSide) goto areaOfSquare;
                     Square square = new(squareSide);
                     square.CalcArea();
                     goto Beginning;
-                    break;
                 case 2:
                 areaOfRectangular:
-                    Console.WriteLine("Please enter width of Rectangular");
+                    Console.WriteLine(ConsoleMessages.RectangularWidth);
                     bool isCorrectWidth = double.TryParse(Console.ReadLine(), out double rectangularWitdh);
-                    Console.WriteLine("Please enter length of Rectangular");
+                    Console.WriteLine(ConsoleMessages.RectangularLength);
                     bool isCorrectLength = double.TryParse(Console.ReadLine(), out double rectangularLength);
                     if (isCorrectWidth && isCorrectLength)
                     {
@@ -44,12 +45,9 @@ namespace InterfaceAbstraction
                 case 3:
                     break;
                 default: goto Beginning;
-
-
-
-
-
             }
+            #endregion
         }
     }
 }
+
